9a17d6b27d7a28e5f7126846f6b49ae2be2e4242
zero219 <303368764@qq.com>
2023-12-10 14:00:21 +0800

    升级Net6

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,68 +1,63 @@
 using Entity.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Autofac;
+using Api;
 
-namespace Api
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+builder.Host.ConfigureLogging((hostBuidleContext, loggingBuilder) =>
 {
-    public class Program
+    //过滤掉System和Microsoft开头的命名空间下的组件产生的警告级别一下的日志
+    loggingBuilder.AddFilter("Microsoft", LogLevel.Warning);
+    loggingBuilder.AddFilter("System", LogLevel.Warning);
+    loggingBuilder.AddLog4Net();
+});
+
+// Autofac容器
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(startup.ConfigureContainer);
+
+builder.Host.ConfigureServices(services =>
+{
+    Console.WriteLine("执行顺序ConfigureServices");
+});
+builder.Host.ConfigureAppConfiguration(config =>
+{
+    Console.WriteLine("执行顺序ConfigureAppConfiguration");
+});
+builder.Host.ConfigureHostConfiguration(builder =>
+{
+    Console.WriteLine("执行顺序ConfigureHostConfiguration");
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                try
-                {
-                    var db = scope.ServiceProvider.GetService<RoutineDbContext>();
-                    db.Database.EnsureDeleted();//删除数据库
-                    db.Database.EnsureCreated();
-
-                }
-                catch (Exception ex)
-                {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "Database Migration Error!");
-                    throw;
-                }
-            }
-            host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureLogging((hostBuidleContext, loggingBuilder) =>
-            {
-                //过滤掉System和Microsoft开头的命名空间下的组件产生的警告级别一下的日志
-                loggingBuilder.AddFilter("Microsoft", LogLevel.Warning);
-                loggingBuilder.AddFilter("System", LogLevel.Warning);
-                loggingBuilder.AddLog4Net();
-            })
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory())//使用Autofac
-            .ConfigureServices(services =>
-            {
-                Console.WriteLine("执行顺序ConfigureServices");
-            })
-            .ConfigureAppConfiguration(config =>
-            {
-                Console.WriteLine("执行顺序ConfigureAppConfiguration");
-            })
-            .ConfigureHostConfiguration(builder =>
-            {
-                Console.WriteLine("执行顺序ConfigureHostConfiguration");
-            })
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                Console.WriteLine("执行顺序ConfigureWebHostDefaults");
-                webBuilder.UseStartup<Startup>();
-            });
+        var db = scope.ServiceProvider.GetService<RoutineDbContext>();
+        db.Database.EnsureDeleted();//删除数据库
+        db.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Database Migration Error!");
+        throw new Exception(ex.Message);
     }
 }
+
+startup.Configure(app, app.Environment);
+
+app.Run();
